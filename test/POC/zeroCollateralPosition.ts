faf4e44d402ef1f9c180d96bf27cca30506c622a
Danny G <dg@DESKTOP-KPQ9E2H.localdomain>
2023-04-24 07:13:12 -0400

    show unclaimable funding fees

--- test/POC/zeroCollateralPosition.ts
+++ test/POC/zeroCollateralPosition.ts
@@ -1,9 +1,10 @@
 import { expect } from "chai";
 import { deployFixture } from "../../utils/fixture";
-import { expandDecimals, decimalToFloat } from "../../utils/math";
+import { expandDecimals, decimalToFloat, bigNumberify } from "../../utils/math";
 import { handleDeposit } from "../../utils/deposit";
 import { OrderType, getOrderCount, getOrderKeys, createOrder, handleOrder, executeOrder } from "../../utils/order";
 import { getAccountPositionCount, getPositionKeys } from "../../utils/position";
+import { handleWithdrawal } from "../../utils/withdrawal";
 import * as keys from "../../utils/keys";
 import { executeLiquidation } from "../../utils/liquidation";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
@@ -11,12 +12,12 @@ import { errorsContract } from "../../utils/error";
 
 describe.only("Guardian", () => {
   let fixture;
-  let user0, user1;
+  let user0, user1, user2;
   let dataStore, reader, ethUsdMarket, wnt, usdc, exchangeRouter;
 
   beforeEach(async () => {
     fixture = await deployFixture();
-    ({ user0, user1 } = fixture.accounts);
+    ({ user0, user1, user2 } = fixture.accounts);
     ({ dataStore, reader, ethUsdMarket, wnt, usdc, exchangeRouter } = fixture.contracts);
 
     await handleDeposit(fixture, {
@@ -28,7 +29,7 @@ describe.only("Guardian", () => {
     });
   });
 
-  it("CRITICAL:Position with size but 0 collateral", async () => {
+  it("CRITICAL: Position with size but 0 collateral breaks funding fees", async () => {
     await dataStore.setUint(keys.fundingFactorKey(ethUsdMarket.marketToken), decimalToFloat(1, 10));
     await dataStore.setUint(keys.fundingExponentFactorKey(ethUsdMarket.marketToken), decimalToFloat(1));
     expect(await getAccountPositionCount(dataStore, user0.address)).eq(0);
@@ -137,10 +138,72 @@ describe.only("Guardian", () => {
       await dataStore.getUint(keys.claimableFundingAmountKey(ethUsdMarket.marketToken, usdc.address, user1.address))
     ).eq("0");
 
-    // We break the market token balance validation and are unable to claim. If more WNT was sent to the market token,
-    // the user would eat into another user's value.
+    // We break the market token balance validation and are unable to claim.
     await expect(
       exchangeRouter.connect(user1).claimFundingFees([ethUsdMarket.marketToken], [wnt.address], user1.address)
+    ).to.be.revertedWithCustomError(errorsContract, "InvalidMarketTokenBalance");
+
+    // If another user earns funding fees, they are also unable to claim
+    // as market token balance validation will continue to fail.
+
+    // User1 will pay funding fees.
+    await handleOrder(fixture, {
+      create: {
+        market: ethUsdMarket,
+        initialCollateralToken: wnt,
+        initialCollateralDeltaAmount: expandDecimals(10, 18),
+        sizeDeltaUsd: decimalToFloat(200 * 1000),
+        acceptablePrice: expandDecimals(5000, 12),
+        orderType: OrderType.MarketIncrease,
+        isLong: true,
+      },
+    });
+
+    // User2 creates a short with size $500 and shall get paid.
+    await handleOrder(fixture, {
+      create: {
+        account: user2,
+        market: ethUsdMarket,
+        initialCollateralToken: wnt,
+        initialCollateralDeltaAmount: expandDecimals(1, 18),
+        sizeDeltaUsd: decimalToFloat(1000),
+        acceptablePrice: expandDecimals(5000, 12),
+        orderType: OrderType.MarketIncrease,
+        isLong: false,
+      },
+    });
+
+    expect(await getAccountPositionCount(dataStore, user2.address)).eq(1);
+
+    // Longs should be paying shorts for this time.
+    await time.increase(60);
+
+    // User2 closes their position and has been earning funding fees for ~60 seconds
+    await handleOrder(fixture, {
+      create: {
+        account: user2,
+        market: ethUsdMarket,
+        initialCollateralToken: wnt,
+        initialCollateralDeltaAmount: 0,
+        swapPath: [],
+        sizeDeltaUsd: decimalToFloat(1000),
+        acceptablePrice: expandDecimals(5000, 12),
+        executionFee: expandDecimals(1, 15),
+        minOutputAmount: 0,
+        orderType: OrderType.MarketDecrease,
+        isLong: false,
+        shouldUnwrapNativeToken: false,
+      },
+    });
+    expect(await getAccountPositionCount(dataStore, user2.address)).eq(0);
+
+    // User1 is unable to claim funding fees.
+    await expect(
+      exchangeRouter.connect(user1).claimFundingFees([ethUsdMarket.marketToken], [wnt.address], user1.address)
+    ).to.be.revertedWithCustomError(errorsContract, "InvalidMarketTokenBalance");
+    // User2 is unable to claim their tiny amount of funding fees.
+    await expect(
+      exchangeRouter.connect(user2).claimFundingFees([ethUsdMarket.marketToken], [wnt.address], user1.address)
     ).to.be.revertedWithCustomError(errorsContract, "InvalidMarketTokenBalance");
   });
 });
